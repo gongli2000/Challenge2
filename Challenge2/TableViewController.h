e84b63aca02a2c697666253aa11366ad8b4a8626
gongli2000 <larrye2000@gmail.com>
2014-02-11 08:23:11 -0800

    ddd

---
+++ Challenge2/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  Challenge2
+//
+//  Created by Larry on 2/11/14.
+//  Copyright (c) 2014 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *titles;
+@property (nonatomic, strong) NSArray *description;
+@end
